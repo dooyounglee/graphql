936f696159ae72d339793d6b009e7763a932b28a
doo <gostbaducking1@nate.com>
2022-10-06 01:01:53 +0900

    fix: optional 사용

--- src/main/java/com/doo/graphql/security/UserDetailsServiceImpl.java
+++ src/main/java/com/doo/graphql/security/UserDetailsServiceImpl.java
@@ -1,11 +1,14 @@
 package com.doo.graphql.security;
 
+import java.util.Optional;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.doo.graphql.dao.UserRepository;
+import com.doo.graphql.vo.User;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,7 +23,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		log.info("com.doo.graphql.security.UserDetailsServiceImpl.loadUserByUsername.email : {}", email);
-		return userRepository.findByEmail(email);
+		
+		Optional<User> oUser = userRepository.findByEmail(email);
+		if (oUser.isEmpty()) {
+			return null;
+		} else {
+			return oUser.get();
+		}
 	}
 
 }
