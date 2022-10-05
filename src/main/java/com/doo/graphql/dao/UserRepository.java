db11807fdfbab9bd73138026e44ccc9a5edbd2fe
doo <gostbaducking1@nate.com>
2022-10-06 01:02:45 +0900

    fix: findByEmail optional로 바꿈

--- src/main/java/com/doo/graphql/dao/UserRepository.java
+++ src/main/java/com/doo/graphql/dao/UserRepository.java
@@ -1,13 +1,17 @@
 package com.doo.graphql.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.doo.graphql.vo.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
-	User findByEmail(String email);
+	//User findByEmail(String email);
 
 	User findByUsername(String username);
+	
+	Optional<User> findByEmail(String email);
 
 }
